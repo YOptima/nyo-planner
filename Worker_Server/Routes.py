64cda7849e04bd817942ed301c01afb32314ae70
YOptima <yash@yoptima.com>
2022-07-14 13:00:58 +0530

    Create Plan Route init

--- Worker_Server/Routes.py
+++ Worker_Server/Routes.py
@@ -1,14 +1,18 @@
-from config import REPORT_ID_CONTEXT
+# Fast API Configurations
+from fastapi import FastAPI, HTTPException, Request
+from h11 import Data
+app = FastAPI()
+
 
 # Logging Configurations
 import logging
 logger = logging.getLogger(__name__)
 
 
-# Fast API Configurations
-from fastapi import FastAPI, HTTPException, Request
-app = FastAPI()
-
+from config import REPORT_ID_CONTEXT
+from RequestModels.WorkerCreatePlanModel import WorkerCreatePlanRequest
+from GoogleSheets.GoogleSheets import GoogleSheets 
+from pandas import DataFrame
 
 # Context Middleware
 @app.middleware("http")
@@ -30,3 +34,33 @@ async def context_middelware(request: Request, call_next):
         assert report_id == REPORT_ID_CONTEXT.get()
         logger.debug("[+] Request Ended")
         return response
+
+@app.post("/worker/create")
+async def createPlan(request: WorkerCreatePlanRequest):
+
+    sheet_id: str = request.sheet_id
+    gsheet_handler: GoogleSheets = GoogleSheets()
+    
+    # Fetching Global Config Sheet Data
+    global_config_sheet_name: str = request.global_config_sheet_name
+    global_config_sheet_dataframe: DataFrame = gsheet_handler.fetchData(
+        spreadsheet_id = sheet_id,
+        sheet_name = global_config_sheet_name
+    )
+    print( global_config_sheet_dataframe )
+
+    # Fetching Config Sheet Data
+    config_sheet_name: str = request.config_sheet_name
+    config_sheet_dataframe: DataFrame = gsheet_handler.fetchData(
+        spreadsheet_id = sheet_id,
+        sheet_name = config_sheet_name
+    )
+    print( config_sheet_dataframe )
+
+    # Fetching Budget Based Data
+    data_sheet_name: str = request.data_sheet_name
+    data_sheet_dataframe: DataFrame = gsheet_handler.fetchData(
+        spreadsheet_id = sheet_id,
+        sheet_name = data_sheet_name  
+    )
+    print( data_sheet_dataframe )
