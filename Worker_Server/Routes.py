140437f8d475d6c6348a25884d46acb7f9c0e572
YOptima <yash@yoptima.com>
2022-07-14 12:04:52 +0530

    Adding Context Middleware

--- Worker_Server/Routes.py
+++ Worker_Server/Routes.py
@@ -1,6 +1,32 @@
-from fastapi import FastAPI
-app = FastAPI()
+from config import REPORT_ID_CONTEXT
 
 # Logging Configurations
 import logging
 logger = logging.getLogger(__name__)
+
+
+# Fast API Configurations
+from fastapi import FastAPI, HTTPException, Request
+app = FastAPI()
+
+
+# Context Middleware
+@app.middleware("http")
+async def context_middelware(request: Request, call_next):
+
+    headers = request.headers
+    report_id = headers.get("X-Request-ID")
+    REPORT_ID_CONTEXT.set(report_id)
+
+    try:
+        response = await call_next( request )
+    except Exception as error:
+        logger.error(f"[-] Request Failed")
+        raise HTTPException(
+            status = 500,
+            detail = "Unable to Serve Request" 
+        )
+    finally: 
+        assert report_id == REPORT_ID_CONTEXT.get()
+        logger.debug("[+] Request Ended")
+        return response
